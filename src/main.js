46b602175160b7bedea7164b25d7a7d042bea5d5
Will <will@eldrmedia.com>
2021-10-25 18:57:18 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+import './styles/app.scss'
 
 Vue.config.productionTip = false
 
